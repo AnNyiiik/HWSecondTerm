dd4631364bba28813b2a38fc5b8fc5b02e52d96f
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2023-02-25 12:12:43 +0300

    HW1_BWT/HW1_BWT/Program.cs
    added input output

--- HW1_BWT/HW1_BWT/Program.cs
+++ HW1_BWT/HW1_BWT/Program.cs
@@ -11,10 +11,45 @@ namespace HW1_BWT
                 Console.WriteLine("Tests have been failed");
                 return;
             }
-            string code = BWT.Encode("banana").Item1;
-            Console.WriteLine(code);
-            string encoded = BWT.Decode(code, 3);
-            Console.WriteLine(encoded);
+            Console.WriteLine("Please, enter the option (1 to encode and 2 to decode):");
+            int option = 0;
+            bool isCorrect = Int32.TryParse(Console.ReadLine(), out option);
+            if (!isCorrect)
+            {
+                Console.WriteLine("Not a number");
+                return;
+            }
+
+            if (option == 1)
+            {
+                Console.WriteLine("Please, enter a string to encode:");
+                string sequence = Console.ReadLine();
+                Tuple<string, int> answer = BWT.Encode(sequence);
+                Console.WriteLine("Encoded string : {0} \nPosition : {1}", answer.Item1, answer.Item2);
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine("Please, enter a string to decode:");
+                string sequence = Console.ReadLine();
+                Console.WriteLine("Please, enter a position:");
+                int position = 0;
+                isCorrect = Int32.TryParse(Console.ReadLine(), out position);
+                if (!isCorrect)
+                {
+                    Console.WriteLine("Not a number");
+                    return;
+                } else if (position >= sequence.Length || position < 0)
+                {
+                    Console.WriteLine("Position is incorrect");
+                    return;
+                }
+                string answer = BWT.Decode(sequence, position);
+                Console.WriteLine("Decoded string : {0} ", answer);
+            }
+            else
+            {
+                Console.WriteLine("Option is incorrect");
+            }
         }
     }
 }
