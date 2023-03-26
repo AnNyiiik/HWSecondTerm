aaf60fbd27311fa3cb17fddf95d1dbb7b4bfaa26
AnNyiiik <anna.nikolaevakons@gmail.com>
2023-03-26 20:21:14 +0300

    fixed mistakes

--- HW2_Calculator/HW2_Calculator.Tests/StackImplementationTests.cs
+++ HW2_Calculator/HW2_Calculator.Tests/StackImplementationTests.cs
@@ -10,45 +10,42 @@ public class StackImplementationTests
         };
 
     [TestCaseSource(nameof(Stacks))]
-    public void PopFromEmptyStackShouldNotFailTest(IStack stack)
+    public void PopFromEmptyStackShouldThrowAccessViolationExceptionTest(IStack stack)
     {
-        stack.Pop();
-        Assert.IsTrue(stack.IsEmpty);
+        Assert.Throws<AccessViolationException>(() => stack.Pop());
     }
 
-    [Test]
-    public void PushShallWork()
+    [TestCaseSource(nameof(Stacks))]
+    public void PushShallWorkTest(IStack stack)
     {
         stack.Push(1);
-
-        Assert.IsFalse(stack.IsEmpty);
+        Assert.That(!stack.IsEmpty());
     }
 
-    [Test]
-    public void PushAndPopShallLeaveStackEmpty()
+    [TestCaseSource(nameof(Stacks))]
+    public void PushAndPopShallLeaveStackEmptyTest(IStack stack)
     {
         stack.Push(1);
         stack.Pop();
-
-        Assert.IsTrue(stack.IsEmpty);
+        Assert.That(stack.IsEmpty());
     }
 
-    [Test]
-    public void TwoPushesAndPopShallLeaveElementInStack()
+    [TestCaseSource(nameof(Stacks))]
+    public void TwoPushesAndPopShallLeaveElementInStackTest(IStack stack)
     {
         stack.Push(1);
         stack.Push(2);
         stack.Pop();
 
-        Assert.IsFalse(stack.IsEmpty);
+        Assert.That(!stack.IsEmpty());
     }
 
-    [Test]
-    public void PushAndPopShallGetExpectedValue()
+    [TestCaseSource(nameof(Stacks))]
+    public void PushAndPopShallGetExpectedValueTest(IStack stack)
     {
         stack.Push(1);
         var value = stack.Pop();
 
-        Assert.AreEqual(1, value);
+        Assert.That(value, Is.EqualTo(1));
     }
 }
