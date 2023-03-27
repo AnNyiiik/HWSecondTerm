4cd4b28aa0646d92d1c10a16bc27b26a05b07e47
AnNyiiik <anna.nikolaevakons@gmail.com>
2023-03-28 00:27:20 +0300

    changed

--- HW2_Calculator/HW2_Calculator/Program.cs
+++ HW2_Calculator/HW2_Calculator/Program.cs
@@ -1,40 +1,40 @@
 ﻿using HW2_Calculator;
 
-    if (!Test.TestStackImplementation())
-    {
-        Console.WriteLine("Test has been failed");
-        return;
-    }
-    Console.WriteLine("To choose a stack based on array press 1\nTo choose a stack based on list press 2\n");
-    var isCorrect = Int32.TryParse(Console.ReadLine(), out var option);
-    if (!isCorrect)
-    {
-        Console.WriteLine("Not a number");
-        return;
-    }
+if (!Test.TestStackImplementation())
+{
+    Console.WriteLine("Test has been failed");
+    return;
+}
+Console.WriteLine("To choose a stack based on array press 1\nTo choose a stack based on list press 2\n");
+var isCorrect = Int32.TryParse(Console.ReadLine(), out var option);
+if (!isCorrect)
+{
+    Console.WriteLine("Not a number");
+    return;
+}
 
-    if (option > 2 || option < 1)
-    {
-        Console.WriteLine("Not an option");
-        return;
-    }
-    StackCalculator calculator;
-    if (option == 1)
-    {
-        var stackArray = new StackBasedOnArray();
-        calculator = new StackCalculator(stackArray);
-    }
-    else
-    {
-        var stackList = new StackBasedOnList();
-        calculator = new StackCalculator(stackList);
-    }
-    Console.WriteLine("enter the expression to calculate:\n");
-    var expression = Console.ReadLine();
-    var result = (expression != null) ? calculator.Calculate(expression) : new Tuple<bool, double?>(false, 0);
-    if (result.Item1 == false)
-    {
-        Console.WriteLine("An expression is incorrect");
-        return;
-    }
-    Console.WriteLine("A result is {0}", result.Item2);
+if (option > 2 || option < 1)
+{
+    Console.WriteLine("Not an option");
+    return;
+}
+StackCalculator calculator;
+if (option == 1)
+{
+    var stackArray = new StackBasedOnArray();
+    calculator = new StackCalculator(stackArray);
+}
+else
+{
+    var stackList = new StackBasedOnList();
+    calculator = new StackCalculator(stackList);
+}
+Console.WriteLine("enter the expression to calculate:\n");
+var expression = Console.ReadLine();
+var result = (expression != null) ? calculator.Calculate(expression) : new Tuple<bool, double?>(false, 0);
+if (result.Item1 == false)
+{
+    Console.WriteLine("An expression is incorrect");
+    return;
+}
+Console.WriteLine("A result is {0}", result.Item2);
